c3f110ea71f7f7717bca689bbe144745fc2473bb
Kevgraham384 <kev.graham384@gmail.com>
2024-09-11 10:16:48 -0700

    fixed some eslint errors

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
 import { Provider } from 'react-redux';
-import store from './store.js';
+import App from './App';
+import store from './store';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
