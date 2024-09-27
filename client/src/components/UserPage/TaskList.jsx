223eb92b0d3dfedcaed3ee0f75b9ddd4ec5c386a
Kevgraham384 <kev.graham384@gmail.com>
2024-09-27 12:46:36 -0700

    fixed some alignment for table headers

--- client/src/components/UserPage/TaskList.jsx
+++ client/src/components/UserPage/TaskList.jsx
@@ -27,8 +27,8 @@ function TaskList({ tasks }) {
               <thead>
                 <tr>
                   <th scope="col" className="px-1 py-2 text-start text-xs font-bold text-black-500 uppercase dark:text-neutral-500">Completed</th>
-                  <th scope="col" className="px-1 py-2 lg:text-start text-center text-xs font-bold text-black-500 uppercase dark:text-neutral-500">Tasks</th>
-                  <th scope="col" className="hidden sm:table-cell px-1 py-2 text-start text-xs font-bold text-black-500 uppercase dark:text-neutral-500">
+                  <th scope="col" className="px-1 py-2 lg:text-start text-start text-xs font-bold text-black-500 uppercase dark:text-neutral-500">Tasks</th>
+                  <th scope="col" className="hidden sm:table-cell px-1 py-2 text-start text-xs font-bold text-black-500 uppercase dark:text-neutral-500 lg:flex">
                     Priority
                     {!viewPriority && (
                       <FontAwesomeIcon
@@ -50,7 +50,7 @@ function TaskList({ tasks }) {
                     )}
                   </th>
                   <th scope="col" className="hidden lg:table-cell px-1 py-2 text-start text-xs font-bold text-black-500 uppercase dark:text-neutral-500">Category</th>
-                  <th scope="col" className="px-1 py-2 text-start text-xs font-bold text-black-500 uppercase dark:text-neutral-500">
+                  <th scope="col" className="px-1 py-2 text-start text-xs font-bold text-black-500 uppercase dark:text-neutral-500 flex">
                     Due Date
                     {!viewDate && (
                       <FontAwesomeIcon
